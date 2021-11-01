2cbe600b01ea70ce528b00db0084a35574dda158
risaaugusta <augustariris20@gmail.com>
2021-11-01 19:47:11 +0700

    init progress 2

---
+++ Assets/Script/Heart System/HeartSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public int life;
+    private bool dead;
+
+    void Start(){
+        life = hearts.Length;
+    }
+
+    void Update()
+    {
+        /*if(life<1){
+            Destroy(hearts[0].gameObject);
+        }else if(life<2){
+            Destroy(hearts[1].gameObject);
+        }else if(life<3){
+            Destroy(hearts[2].gameObject);
+        }*/
+        if(dead == true){
+            Debug.Log("dead");
+            Invoke("TurnOffGameObject",0f);
+        }
+    }
+
+    public void TakeDamage(){
+        life -= 1;
+        Destroy(hearts[life].gameObject);
+        if(life < 1){
+            dead = true;
+        }
+    }
+
+    void TurnOffGameObject(){
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D target){
+        if(target.tag == "Enemy"){
+            Debug.Log("I'm Hit");
+            TakeDamage();
+        }
+    }
+}
---
+++ Assets/Script/Score Manager/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake(){
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = score.ToString() + " POINTS";
+        highscoreText.text = "Highscore: " + highscore.ToString();
+    }
+
+    public void AddPoint()
+    {
+        score += 5;
+        scoreText.text = score.ToString() + " POINTS";
+        if(highscore<score){
+            PlayerPrefs.SetInt("highscore", score);
+        }
+        
+    }
+}
